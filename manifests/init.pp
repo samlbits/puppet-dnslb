96350a0df988f0f5e746dbeaa1b0ef6b2b0df732
Leif Johansson <leifj@sunet.se>
2013-07-31 13:38:05 +0200

    install xmpp support

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,6 +8,9 @@ class dnslb ($dir = '/opt/dnslb', $zone = 'example.com.json',$config = 'example.
   python::virtualenv { $dir:
     ensure => present
   }
+  python::pip { 'xmpppy':
+    virtualenv => $dir
+  }
   python::pip { 'python-dnslb':
     virtualenv => $dir
   }
